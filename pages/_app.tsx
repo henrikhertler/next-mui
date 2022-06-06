8cbf8ec6f563b6da7119f6eba84679e6db26a193
Henrik <henrik@bitfactory.io>
2022-06-06 15:51:57 +0200

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import createEmotionCache from '../src/utility/createEmotionCache';
+import theme from '../src/utility/theme';
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+	const {
+		Component,
+		emotionCache = clientSideEmotionCache,
+		pageProps,
+	} = props;
+
+	return (
+		<CacheProvider value={emotionCache}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	);
+};
+
+export default MyApp;
